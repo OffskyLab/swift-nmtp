d323a14b0e91e3ec3e1a290b58b6d2cf5a3510ed
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-05 02:25:53 +0800

    [ADD] Task 1: Create swift-nmtp repo with Package.swift
    Initialise swift-nmtp as a standalone Swift package with NMTP target,
    NMTPTests test target, and dependencies on swift-nio, swift-nio-extras,
    MessagePacker, and swift-log.

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "swift-nmtp",
+    platforms: [.macOS(.v13)],
+    products: [
+        .library(name: "NMTP", targets: ["NMTP"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.40.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.0"),
+        .package(url: "https://github.com/hirotakan/MessagePacker.git", from: "0.4.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
+    ],
+    targets: [
+        .target(name: "NMTP", dependencies: [
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOExtras", package: "swift-nio-extras"),
+            .product(name: "MessagePacker", package: "MessagePacker"),
+            .product(name: "Logging", package: "swift-log"),
+        ]),
+        .testTarget(name: "NMTPTests", dependencies: ["NMTP"]),
+    ]
+)
