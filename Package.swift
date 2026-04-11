ae67652c7a3cf2f758df24623c77716845ef9f8e
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-11 16:41:14 +0800

    chore: remove stale MessagePacker package dependency from root manifest

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.40.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.0"),
-        .package(url: "https://github.com/hirotakan/MessagePacker.git", from: "0.4.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
     ],
     targets: [
